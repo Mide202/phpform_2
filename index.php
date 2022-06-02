b52ee31736eea59271c1f4dcb9c44556871c0a32
Mide202 <abodunrinogunsanya01.com>
2022-06-02 10:12:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>form get</title>
+</head>
+<body>
+    <form method="POST" action="user_data.php">
+        <label for="name">
+            Name: <input type="text" name="name">
+        </label> <br> 
+        <label for="email">
+            email: <input type="email" name="email">
+        </label> <br>
+        <label for="Dob">
+            Date of birth: <input type="Date of Birth" name="Dob">
+        </label> <br>
+        <label for="Gender">
+            Gender: <input type="Gender" name="Gender">
+        </label> <br>
+        <label for="Country">
+            Country: <input type="Country" name="Country">
+        </label> <br>
+        <label for="Religion">
+            Country: <input type="Religion" name="Religion">
+        </label> <br>
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ user_data.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$Dob = $_POST['Dob'];
+$Gender = $_POST['Gender'];
+$Country = $_POST['Country'];
+
+
+
+echo "Name" . $name . "<br>";
+echo "email" . $email . "<br>";
+echo "Dob" . $Dob . "<br>";
+echo "Gender" . $Gender . "<br>";
+echo "Country" . $Country . "<br>";
+echo "Religion" . $Christian . "<br>";
+  ?>
